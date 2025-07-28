1c642248699e2c20d677a1ab4f70b7a5abd58e16
Pankaj494-hue <pankajkumar100995@gmail.com>
2025-07-28 15:28:24 +0530

    Add files via upload

---
+++ AccountMapper.java
@@ -0,0 +1,24 @@
+package net.javaguides.banking_appTest.mapper;
+
+import net.javaguides.banking_appTest.dto.AccountDto;
+import net.javaguides.banking_appTest.entity.Account;
+
+public class AccountMapper {
+    public static Account mapToAccount(AccountDto accountDto){
+        Account account=new Account(
+                accountDto.id(),
+                accountDto.accountHolderName(),
+                accountDto.balance()
+        );
+        return account;
+    }
+
+    public static AccountDto mapToAccountDto(Account account){
+        AccountDto accountDto=new AccountDto(
+                account.getId(),
+                account.getAccountHolderName(),
+                account.getBalance()
+        );
+        return accountDto;
+    }
+}
