e033db011107efa1030ded7344899d6a23adfd7d
Pankaj494-hue <pankajkumar100995@gmail.com>
2025-07-28 15:25:48 +0530

    Add files via upload

---
+++ BankingAppTestApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.banking_appTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppTestApplication.class, args);
+	}
+
+}
---
+++ BankingAppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.banking_appTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
